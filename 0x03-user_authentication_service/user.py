5f5153c3073feab8f0d72e39a98376b9e3ec40d4
Jamescog <jamescog72@gmail.com>
2023-03-15 20:33:49 +0300

    orm

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,9 +1,10 @@
+#!/usr/bin/evn python3
 """0-user-model
 """
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
