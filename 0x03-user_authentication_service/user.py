4301ce2e008d97e19fa642638cea732fb39bf87e
Jamescog <jamescog72@gmail.com>
2023-03-15 20:36:00 +0300

    shebang line fixed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,10 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 """0-user-model
 """
 
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
