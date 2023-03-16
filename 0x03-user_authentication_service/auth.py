80e5a0d3640efa15c3cc75b4a21bc641e64e8ae7
Jamescog <jamescog72@gmail.com>
2023-03-16 19:54:29 +0300

    hashing implemeted

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""4. Hash password"""
+
+
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """returns a salted hash of the input password"""
+    salt = gensalt()
+    return hashpw(password, salt)
