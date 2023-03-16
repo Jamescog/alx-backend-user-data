af0575227e064afb7652fa397469c17088e0b4b3
Jamescog <jamescog72@gmail.com>
2023-03-16 22:14:04 +0300

    session_id creator

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
             return checkpw(password.encode(), hashed)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """returns session id as string"""
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=uuid4)
+            return uuid4
+        except NoResultFound:
+            return None
