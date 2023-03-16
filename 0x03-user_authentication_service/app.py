5e3e610104f61c2d54ac8a1cb3d22863147eae45
Jamescog <jamescog72@gmail.com>
2023-03-16 12:30:46 +0300

    simple appicaltion setup

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""simple flask app to handle authorization and authentication
+"""
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """index route"""
+    return jsonify({"message": "Bienvenue"})
